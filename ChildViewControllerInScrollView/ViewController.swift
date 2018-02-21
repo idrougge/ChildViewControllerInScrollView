c0a269580d9c5045ecc2f697a6c575cd2200ae56
Iggy Drougge <idrougge@mac.com>
2018-02-21 16:58:16 +0100

    Initial Commit

---
+++ ChildViewControllerInScrollView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ChildViewControllerInScrollView
+//
+//  Created by Iggy Drougge on 2018-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
